2957fe62554d3b61e598b791ff44296d2e1703e4
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-11 14:14:35 -0700

    added test for no input parameters and refactored code

--- sum.js
+++ sum.js
@@ -1,10 +1,8 @@
 function sum(firstNumber, secondNumber){
-    if(typeof(firstNumber) !== 'number' || typeof(secondNumber) !== 'number'){
+    if(typeof(firstNumber) !== 'number' || typeof(secondNumber) !== 'number' || isNaN(firstNumber) || isNaN(secondNumber)){
         return 'Invalid input(s)';
     }
-    if(isNaN(firstNumber) || isNaN(secondNumber)){
-        return 'Not a Number';
-    }
+
     return firstNumber + secondNumber;
 }
 
--- sum.test.js
+++ sum.test.js
@@ -1,6 +1,6 @@
 const summation = require('./sum');
 
-test('No input parameters', ()=>{
+test('No input parameters(Empty Parameters)', ()=>{
     expect(summation()).toBe('Invalid input(s)');
 })
 
@@ -8,14 +8,14 @@ test('adds 1 + 2 to equal 3', ()=>{
     expect(summation(1,2)).toBe(3);
 });
 
-test('adds 0.1 + 0.2 to equal 3', ()=>{
+test('adds 0.1 + 0.2 to equal 3(Rounding Floats)', ()=>{
     expect(summation(0.1,0.2)).toBeCloseTo(0.3);
 });
 
 test('firstNumber and secondNumber are NaN',()=>{
-    expect(summation(NaN,NaN)).toEqual('Not a Number')
+    expect(summation(NaN,NaN)).toEqual('Invalid input(s)')
 });
 
 test('firstNumber and secondNumber are not real numbers', ()=>{
-    expect(summation('a',1)).toEqual('Invalid input(s)');
+    expect(summation('a','a')).toEqual('Invalid input(s)');
 });
