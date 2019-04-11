65d42d8f003cc445f39faddca348eb45ce6dc7cd
eofafrica4lyf <aboderinemmanuel@gmail.com>
2019-04-11 10:51:23 -0700

    added tests for rounded up floats and no input parameters

--- sum.js
+++ sum.js
@@ -2,6 +2,9 @@ function sum(firstNumber, secondNumber){
     if(typeof(firstNumber) !== 'number' || typeof(secondNumber) !== 'number'){
         return 'Invalid input(s)';
     }
+    if(isNaN(firstNumber) || isNaN(secondNumber)){
+        return 'Not a Number';
+    }
     return firstNumber + secondNumber;
 }
 
